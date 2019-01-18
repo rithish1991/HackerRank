f74c652829356a1811294c7a98d9f33ddf97bb7f
RRAMAC22 <RRAMAC22@WGC10140DMX52.chennai.ford.com>
2019-01-18 16:11:07 +0530

    push

--- HackerRank/src/Com/Practice/Algorithms/Warmup/BirthdayCakeCandles.java
+++ HackerRank/src/Com/Practice/Algorithms/Warmup/BirthdayCakeCandles.java
@@ -20,6 +20,7 @@ public class BirthdayCakeCandles {
 		return Collections.max(data.values());
 
 	}
+
 	public static void main(String[] args) {
 		int arr[] = { 3, 2, 1, 3, 2, 2, 2, 3 };
 		System.out.println(birthdayCakeCandles(arr));
